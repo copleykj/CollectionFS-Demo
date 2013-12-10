2f856f35dcb737318e3c03012225a1bfdf0d7bdc
Kelly Copley <copleykj@gmail.com>
2013-12-10 13:37:36 -0500

    client side images collection

---
+++ client/collections/images.js
@@ -0,0 +1,14 @@
+Images = new FS.Collection("images", {
+    useHTTP: true,
+    
+    filter: {
+        maxSize: 10485760, //in bytes
+        allow: {
+            contentTypes: ['image/*'],
+            extensions: ['png', 'PNG', 'jpg', 'JPG', 'jpeg', 'JPEG', 'gif', 'GIF']
+        },
+        onInvalid: function (message) {
+            alert(message);
+        }
+    }
+});
